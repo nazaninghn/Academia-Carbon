6aa6a5f0b5061d79c5baf504de5022d50305b553
nazanintp <nazaninghafouriann@gmail.com>
2025-11-24 08:41:46 -0800

    feat: Production ready - Render deployment + security updates

--- carbon_tracker/settings.py
+++ carbon_tracker/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-hb@0-4*53iza=(08tohqr@v5=^&6(66dwsu1xdla0g%ppcyxmt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default='False') == 'True'
 
 # ALLOWED_HOSTS configuration
 allowed_hosts_str = config('ALLOWED_HOSTS', default='127.0.0.1,localhost')
