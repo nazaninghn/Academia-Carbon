2981561f66a91aeaacd0b924b6fc184d61b06c40
nazanintp <nazaninghafouriann@gmail.com>
2026-01-14 10:25:33 -0800

    fix: Remove empty SQL query from email unique migration
    - Fixed ProgrammingError: can't execute an empty query
    - Removed comment-only SQL block
    - Added WHERE clause to handle empty emails

--- ghg/migrations/0002_make_email_unique.py
+++ ghg/migrations/0002_make_email_unique.py
@@ -6,24 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ghg', '0001_initial'),  # Update this to your last migration
+        ('ghg', '0001_initial'),
     ]
 
     operations = [
-        # First, ensure no duplicate emails exist
-        migrations.RunSQL(
-            sql="""
-                -- This will fail if duplicates exist, which is good!
-                -- Run fix_duplicate_emails command first if this fails
-            """,
-            reverse_sql=migrations.RunSQL.noop,
-        ),
-        
         # Add unique constraint to email field in auth_user table
         migrations.RunSQL(
             sql="""
                 CREATE UNIQUE INDEX IF NOT EXISTS auth_user_email_unique 
-                ON auth_user(email);
+                ON auth_user(email) WHERE email != '';
             """,
             reverse_sql="""
                 DROP INDEX IF EXISTS auth_user_email_unique;
